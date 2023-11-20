7943d3ea70ad69a77a89370c41d0d319366953e3
tree478 <ssshel25@students.shrewsbury.k12.ma.us>
2023-11-20 06:16:00 -0500

    Added database

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ import {dirname} from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import pg from "pg";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+var data = "";
+const db  = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "banking_app_users",
+    password: "Soumya12",
+    port: 5432
 })
 
-app.post("/welcome.html", (req, res) =>{
-    res.sendFile(__dirname + "/public/welcome.html")
+db.connect();
+
+db.query("SELECT * FROM users", (err, res) => {
+    if (err){
+        console.error("Error executing query", err.stack);
+    } else{
+        data = res.rows;
+    }
+    db.end();
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.ejs");
+})
+
+app.post("/welcome.ejs", (req, res) =>{
+    res.sendFile(__dirname + "/public/welcome.ejs")
 })
 
 app.listen(port, () => {
